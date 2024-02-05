39456867e32024b88a24a4bcb1d91f04e747b763
harishreddydontham <harishreddydontham@gmail.com>
2024-02-05 11:46:26 -0600

    small error replaced

--- memory_utilization.sh
+++ memory_utilization.sh
@@ -5,7 +5,7 @@ used_space=$(expr $total_mem - $total_avl)
 echo "the total memory available is ${total_mem}MB and the current utilization is ${used_space} and the available space is ${total_avl}"
 x=$(echo "scale=2; $total_avl / $total_mem" | bc | tr -d '.')
 echo "the free memory percentage is ${x}%"
-if [ x -lt 10 ]
+if [ $x -lt 10 ]
 then
 echo "memory is too low utilized by more than 90%"
 else
