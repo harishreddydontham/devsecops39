6a02c75d5615d2a237209157931515cd11ac4a7b
harishreddydontham <harishreddydontham@gmail.com>
2024-02-08 11:44:04 -0600

    user_automation

--- CheckNumberIsEven.sh
+++ CheckNumberIsEven.sh
@@ -1,8 +1,8 @@
 #checking whether the number is even or not.
 for i in {1..10}; do
-if [ $(expr $i % 2) -eq 0 ]; then
-echo "$i is even"
-else
-echo "$i is odd"
-fi
+    if [ $(expr $i % 2) -eq 0 ]; then
+        echo "$i is even"
+    else
+        echo "$i is odd"
+    fi
 done
---
+++ user_automation.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+slack_web='https://hooks.slack.com/services/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'
+if [ $# -gt 0 ]; then
+    for user in $@; do
+        check_user=$(cat /etc/passwd | grep -w $user | cut -d ":" -f1)
+        if [ "$user" = "$check_user" ]; then
+            echo "The user already exists in the system"
+        else
+            echo "lets create a new user"
+            sudo useradd -m $user --shell /bin/bash -d /home/$user
+            sudo usermod -a -G root $user
+            echo '${user} ALL=(ALL) NOPASSWD: ALL' >>/etc/sudoers
+            spl=$(echo '!@#$%^&*()' | fold -w1 | shuf | head -1 | tail -1)
+            password=$(echo India${spl}${RANDOM})
+            echo "${user}:${password}" | sudo chpasswd
+            passwd -e ${user}
+            echo "The Credentials are username is : ${user}  andd the password : ${password}"
+            curl -X POST ${slack_web} -sL -H 'Content-type: application/json' --data "{"text": \"Username is : ${user}\"}" >>/dev/null
+            curl -X POST ${slack_web} -sL -H 'Content-type: application/json' --data "{"text": \"Password is : ${password} reset it immediately\"}" >>/dev/null
+
+        fi
+    done
+else
+    echo "Give atleast one parameter to create a user"
+fi
